0b14f6c361775c74c39d9abe039418e079f09269
anishuno <anishkhandare1206@gmail.com>
2025-12-26 12:14:40 +0530

    Projeect commands run

--- backend/app/services/forecast_service.py
+++ backend/app/services/forecast_service.py
@@ -2,7 +2,7 @@ import joblib
 import numpy as np
 
 # Load model once (IMPORTANT)
-model = joblib.load("C:/satcode/latest/Final_year/Powergrid-inventory-forecasting/ml/inventory_forecast_model.pkl")
+model = joblib.load("D:\Projects\Final_year_project\Powergrid-inventory-forecasting\ml\inventory_forecast_model.pkl")
 
 def predict_demand(day: int):
     prediction = model.predict(np.array([[day]]))
